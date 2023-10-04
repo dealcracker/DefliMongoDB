a8abf92d6eb4912ddeac515b403b7d81804a1c4d
dealcracker <ken@roundhousesoftware.com>
2023-10-03 21:51:47 -0400

    Updated InstallMongo to add improved handling of Python

--- installMongo.sh
+++ installMongo.sh
@@ -1,56 +1,81 @@
 #!/bin/bash
 # install the Defli MongoDB connector
 # Copyright (c) 2023 dealcracker
+#
+# last modfied: 2020-Oct-3
 
+
+#enforce sudo
 if ! [[ $EUID = 0 ]]; then
     echo "Please run this script with 'sudo'..."
     exit 1
 fi
 
+#change to user's home dir
+cd ~/
+
+echo "============ MongoDB Connector ==============="
+echo "Installing the MongoDB connector for Defli." 
+
 #Determine IP address to use in config.py
 #test if localhost is reachable
-echo "Determining local IP address..."
-ping -c 3 localhost > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	ip_address="localhost"
-	echo "Using localhost instead of fixed IP address"
-else
-	# get the eth0 ip address
-	ip_address=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d'/' -f1)
-	#Check if eth0 is up and has an IP address
-	if [ -n "$ip_address" ]; then
-	  echo "Using wired ethernet IP address: $ip_address"
-	else
-		# Get the IP address of the Wi-Fi interface using ip command
-		ip_address=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d'/' -f1)
-		if [ -n "$ip_address" ]; then
-			echo "Using wifi IP address: $ip_address"
-		else
-			echo "Unable to determine your device IP address"
-			echo "No IP address. Installation Failed."
-			exit 1
-		fi
-	fi
-fi
+  echo "Determining local IP address..."
+  ping -c 3 localhost > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    ip_address="localhost"
+    echo "Using localhost instead of fixed IP address"
+  else
+    # get the eth0 ip address
+    ip_address=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d'/' -f1)
+    #Check if eth0 is up and has an IP address
+    if [ -n "$ip_address" ]; then
+      echo "Using wired ethernet IP address: $ip_address"
+    else
+      # Get the IP address of the Wi-Fi interface using ip command
+      ip_address=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d'/' -f1)
+      if [ -n "$ip_address" ]; then
+        echo "Using wifi IP address: $ip_address"
+      else
+        echo "Unable to determine your device IP address"
+        echo "No IP address. Installation Failed."
+        exit 1
+      fi
+    fi
+  fi
 
+echo ""
 echo "Updating package list..."
 # Update the package list
 apt update -y
 
+echo ""
 echo "Cloning ABS-D data collector for MongoDB..."
 #clone adsb-data-collector-mongodb
 git clone https://github.com/dbsoft42/adsb-data-collector-mongodb.git
 
-echo "Installing dependencies..."
+echo ""
+echo "Installing Python..."
 # Install python3-pip
 apt install -y python3-pip
 
 #patch coroutines if needed (experimental)
-cp /usr/lib/python3.10/asyncio/coroutines.py  /usr/lib/python3.11/asyncio/coroutines.py 2>/dev/null
+#Attempt 1
+if [ -e "/usr/lib/python3.10/asyncio/coroutines.py" ]; then
+  mv /usr/lib/python3.11/asyncio/coroutines.py /usr/lib/python3.11/asyncio/coroutines.py20230927 > /dev/null 2>&1
+  cp /usr/lib/python3.10/asyncio/coroutines.py /usr/lib/python3.11/asyncio/coroutines.py > /dev/null 2>&1
+fi
+#Attempt 2
+if [ -e "/snap/core22/867/usr/lib/python3.10/asyncio/coroutines.py" ]; then
+  mv /usr/lib/python3.11/asyncio/coroutines.py /usr/lib/python3.11/asyncio/coroutines.py20230927 > /dev/null 2>&1
+  cp /snap/core22/867/usr/lib/python3.10/asyncio/coroutines.py /usr/lib/python3.11/asyncio/coroutines.py > /dev/null 2>&1
+fi
 
 # Istall pip modules
+echo ""
+echo "Installing Pip Modules..."
 pip3 install aiohttp motor pymongo python-dateutil dnspython
 
+echo ""
 echo "Preparing the connector config.py file..."
 # copy the default config.py template
 cd adsb-data-collector-mongodb
@@ -71,6 +96,7 @@ sed -i "s|$original_line1|$new_line1|g" "config.py"
 sed -i "s|$original_line2|$new_line2|g" "config.py"
 sed -i "s|$original_line3|$new_line3|g" "config.py"
 
+echo ""
 echo "Preparing the adsb_collector service..."
 #get the working directory
 current_dir=$(pwd)
@@ -109,32 +135,44 @@ echo "WantedBy=multi-user.target" >> /lib/systemd/system/adsb_collector.service
 systemctl enable adsb_collector 
 systemctl start adsb_collector
 
+echo ""
 echo "Waiting for service to start..."
 sleep 3
 
-service_name="adsb_collector"
-
 #get the service status
+service_name="adsb_collector"
 status=$(systemctl is-active "$service_name")
 
 # Check the status
+echo ""
 case "$status" in
   "active")
-    echo "Installation Complete. $service_name is running."
-    echo "You can enter 'sudo systemctl status adsb_collector' to check the status."
+    echo "************************************"
+    echo "Installation Completed Successfully!" 
+    echo "$service_name is running properly."
+    echo 
+    echo "Note: enter 'sudo systemctl status adsb_collector' anytime to check the status."
     ;;
   "inactive")
-    echo "Installation Complete. Warning: $service_name is not running."
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Installation Finished with a warning." 
+    echo "Warning: $service_name is not running."
+    echo ""
     echo "Try entering 'sudo systemctl start adsb_collector' to start the service."
     echo "Then enter 'sudo systemctl status adsb_collector' to check the status."
     ;;
   "failed")
-    echo "Installation Complete. Error: $service_name is in a failed state."
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Installation Finished but failed to start." 
+    echo "Error: $service_name is in a failed state."
+    echo ""
     echo "Try entering 'sudo systemctl start adsb_collector' to start the service."
     echo "Then enter 'sudo systemctl status adsb_collector' to check the status."
     ;;
   *)
-    echo "Installation Complete. Status of $service_name: $status"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Installation Complete." 
+    echo "Status of $service_name: $status"
     echo "You can enter 'sudo systemctl status adsb_collector' to check the status."
     ;;
 esac
