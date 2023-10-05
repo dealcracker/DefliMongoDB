33981eb3cf804a1a218d6d0aaadac5c1bc0e7de5
dealcracker <ken@roundhousesoftware.com>
2023-10-04 21:35:49 -0400

    Updated install script with Bigmaxi’s unbuntu fix

--- installDefli.sh
+++ installDefli.sh
@@ -102,10 +102,11 @@ if [ -e "/snap/core22/867/usr/lib/python3.10/asyncio/coroutines.py" ]; then
   cp /snap/core22/867/usr/lib/python3.10/asyncio/coroutines.py /usr/lib/python3.11/asyncio/coroutines.py > /dev/null 2>&1
 fi
 
-# Istall pip modules
-echo ""
-echo "Installing Pip Modules..."
-pip3 install aiohttp motor pymongo python-dateutil dnspython
+#BM - Changed to this code section to fix ubuntu
+apt install python3-aiohttp -y 
+apt install python3-motor -y
+apt install python3-dateutil -y
+apt install python3-dnspython -y
 
 echo ""
 echo "Preparing the connector config.py file..."
